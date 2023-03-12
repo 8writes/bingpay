b2dbe818eda57780e3e04a026eecd5d41d197a73
Chisom Emmanuel <mailemmanuel00@gmail.com>
2023-03-12 20:48:29 +0100

    update

--- app/components/mobileNav.jsx
+++ app/components/mobileNav.jsx
@@ -1,11 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./mobileNav.module.css";
+import { useEffect } from "react";
+import { Quicksand } from "next/font/google";
 
-export default function MobileNav() {
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  variable: "--font-quicksand",
+  display: "swap",
+});
+
+export default function mobileNav() {
+  useEffect(() => {
+    const mobileNav = document.querySelector(`.${styles.mobileNav}`);
+    // Add or remove the "mobileScrollNav" class based on the scroll position
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        mobileNav.classList.add(styles.mobileScrollNav);
+      } else {
+        mobileNav.classList.remove(styles.mobileScrollNav);
+      }
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <div>
-          <section className={styles.mobileNav}>
-
+    <div className={`${quicksand.variable}`}>
+      <section className={styles.mobileNav}>
+        <div>
+          <Image
+            src="/logo-dark.webp"
+            width={130}
+            height={130}
+            alt="Bingpay Logo"
+          />
+        </div>
+        <div className={styles.mobileNavLinks}>
+         
+        </div>
       </section>
     </div>
   );
