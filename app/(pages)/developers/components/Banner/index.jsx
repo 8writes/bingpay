34c130a7353fa78f0ade6e690faf0b566be65a06
Chisom Emmanuel <mailemmanuel00@gmail.com>
2023-05-19 23:07:59 +0100

    update don land

--- app/(pages)/developers/components/Banner/index.jsx
+++ app/(pages)/developers/components/Banner/index.jsx
@@ -4,20 +4,77 @@ import dynamic from "next/dynamic";
 
 import Image from "next/image";
 import styles from "./index.module.css";
+import Developer from "/public/images/developer.png";
+import ArrowRight from "/public/images/arrow-up-right.svg";
+import Link from "next/link";
 
 const FadeInUp = dynamic(() => import("./Framer/FadeInUp"), {
   ssr: false,
 });
 
-export default function Banner() {
+const FadeInRight = dynamic(() => import("./Framer/FadeInRight"), {
+  ssr: false,
+});
+
+export default function Hero() {
   return (
     <div>
-      <div className={styles.banner}>
-        <div className={styles.bannerContext}>
-          <h1>Developers</h1>
-          <p></p>
+      <section className={styles.Hero}>
+        <div className={styles.heroBg}>
+          <div className={styles.heroLeft}>
+            <div>
+              <h1>Seamless Integrations with Bingpay APIs.</h1>
+
+              <p>
+                Our well-documented APIs lets you create and launch simple or
+                complex financial products.
+              </p>
+            </div>
+
+            <div className={styles.heroSocials}>
+              {" "}
+              <div>
+                {" "}
+                <span className={styles.btn}>
+                  <FadeInUp>
+                    <Link
+                      href="https://documenter.getpostman.com/view/17112744/U16nKPpt"
+                      target="_blank"
+                    >
+                      <button>
+                        Documentation{" "}
+                        <Image
+                          src={ArrowRight}
+                          alt="Picture of the author"
+                          width={20}
+                          height={20}
+                          priority
+                          // blurDataURL="data:..." automatically provided
+                          // placeholder="blur" // Optional blur-up while loading
+                        />{" "}
+                      </button>
+                    </Link>
+                  </FadeInUp>
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.heroImage}>
+            <FadeInUp>
+              <Image
+                src={Developer}
+                alt="Picture of the author"
+                width={300}
+                height={300}
+                priority
+                // blurDataURL="data:..." automatically provided
+                // placeholder="blur" // Optional blur-up while loading
+              />{" "}
+            </FadeInUp>
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
