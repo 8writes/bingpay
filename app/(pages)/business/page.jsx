d22f68045b52cd57fdf6e485afba28edfa164d93
Chisom Emmanuel <mailemmanuel00@gmail.com>
2023-05-27 13:08:06 +0100

    update

--- app/(pages)/business/page.jsx
+++ app/(pages)/business/page.jsx
@@ -1,11 +1,9 @@
 "use client";
 
 import dynamic from "next/dynamic";
-
 const Banner = dynamic(() => import("./components/Banner/index"), {
   ssr: false,
 });
-
 const Section = dynamic(() => import("./components/Section/index"), {
   ssr: false,
 });
