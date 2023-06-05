f1439914b0abc3dbaf57f8d1703b907113fe12f9
Chisom Emmanuel <mailemmanuel00@gmail.com>
2023-06-05 16:53:56 +0100

    update

--- app/(pages)/help-center/lib/Section/index.jsx
+++ app/(pages)/help-center/lib/Section/index.jsx
@@ -51,7 +51,7 @@ export default function Section() {
                 <div class={styles.container}>
                   <FadeInUp>
                     <details>
-                      <summary>BVN verification Failed ?</summary>
+                      <summary>BVN verification failed ?</summary>
                       <p>
                         This is because the names on your bingpay account
                         doesn't match the data on your BVN. Please contact our
@@ -71,27 +71,27 @@ export default function Section() {
                   <FadeInUp>
                     <details>
                       <summary>
-                        How long does withdrawals takes to reflect ?
+                        How long does a withdrawal takes to reflect ?
                       </summary>
                       <p>
-                        Withdrawals will reflect within 3 to 6 working hours.
+                        Withdrawals will reflect within 30 minutes.
                       </p>
                     </details>
                   </FadeInUp>
                   <FadeInUp>
                     <details>
                       <summary>
-                        How long does Airtime to cash transactions take to reflect ?
+                        How long does airtime to cash transactions take to reflect ?
                       </summary>
                       <p>
-                        Airtime to cash transaction are processed within 1 working hour.
+                        Airtime to cash transactions are processed within 1 to 2 working hour.
                       </p>
                     </details>
                   </FadeInUp>
                   <FadeInUp>
                     <details>
                       <summary>
-                        I was debited but wasn't credited on bingPay.
+                        I was debited but wasn't credited on BingPay.
                       </summary>
                       <p>
                         Kindly Contact our Support team via Live chat or any of
@@ -108,7 +108,7 @@ export default function Section() {
                 <div class={styles.container}>
                   <FadeInUp>
                     <details>
-                      <summary>How to Create an Account ?</summary>
+                      <summary>How to create an account ?</summary>
                       <p>
                         Download the bingPay Mobile app, select create an
                         account, fill in out your information and proceed.{" "}
@@ -117,7 +117,7 @@ export default function Section() {
                   </FadeInUp>
                   <FadeInUp>
                     <details>
-                      <summary>How to fund my Account ?</summary>
+                      <summary>How to fund my account ?</summary>
                       <p>
                         Navigate to Fund Wallet, choose from the option,
                         Transfer or Fund with debit card , Input the amount you
@@ -137,7 +137,7 @@ export default function Section() {
                   </FadeInUp>
                   <FadeInUp>
                     <details>
-                      <summary>How to Change my password ?</summary>
+                      <summary>How to change my password ?</summary>
                       <p>
                         Navigate to Account, change password, Input old password
                         and new password.
@@ -146,7 +146,7 @@ export default function Section() {
                   </FadeInUp>
                   <FadeInUp>
                     <details>
-                      <summary>How to Change my Transaction Pin ?</summary>
+                      <summary>How to change my transaction pin ?</summary>
                       <p>
                         Navigate to Account, change Transaction Pin, Input
                         password and new pin.
@@ -155,7 +155,7 @@ export default function Section() {
                   </FadeInUp>
                   <FadeInUp>
                     <details>
-                      <summary>How to access my Sim Services ?</summary>
+                      <summary>How to access my sim services ?</summary>
                       <p>
                         Check Airtime balance — 310
                         <br></br> Data plan — 312 <br></br> Share services — 321{" "}
